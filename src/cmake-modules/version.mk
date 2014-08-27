d662f4cc0be0b0364a945a329602b1d2b122b331
Jim Hayes <jhayes@sdsc.edu>
2014-08-27 12:49:39 -0700

    Adapt Rocks approach to determining the modulefile VERSION value.

--- src/cmake-modules/version.mk
+++ src/cmake-modules/version.mk
@@ -1,3 +1,9 @@
+# Get the cmake version from its version.mk file. See Makefile
+VERSION.MK.MASTER = version.mk
+VERSION.MK.MASTER.DIR = ../cmake
+VERSION.MK.INCLUDE = cmake.version.mk
+include $(VERSION.MK.INCLUDE)
+
 NAME	= cmake-modules
 VERSION	= 3.0.0
 RELEASE	= 0
