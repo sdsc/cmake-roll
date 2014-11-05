951c588efaa4465fb12f60a71487658c81062aeb
Jim Hayes <jhayes@sdsc.edu>
2014-11-05 12:16:56 -0800

    Shift to using build facilities from sdsc-roll.

--- src/cmake/version.mk
+++ src/cmake/version.mk
@@ -1,11 +1,11 @@
-NAME	       = cmake
+NAME	       = sdsc-cmake
 VERSION	       = 3.0.2
-RELEASE	       = 0
+RELEASE	       = 1
 PKGROOT        = /opt/cmake
 
 SRC_SUBDIR     = cmake
 
-SOURCE_NAME    = $(NAME)
+SOURCE_NAME    = cmake
 SOURCE_SUFFIX  = tar.gz
 SOURCE_VERSION = $(VERSION)
 SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
--- src/roll-test/version.mk
+++ src/roll-test/version.mk
@@ -1,6 +1,6 @@
-NAME       = cmake-roll-test
+NAME       = sdsc-cmake-roll-test
 VERSION    = 1
-RELEASE    = 0
+RELEASE    = 1
 PKGROOT    = /root/rolltests
 
 RPM.EXTRAS = AutoReq:No
